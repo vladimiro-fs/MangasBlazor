90a99df5958b1d06bb85ecab307565c2a749c113
vladimiro-fs <vladzyo.gaming@pm.me>
2025-02-12 01:00:27 +0000

    ApiAuthenticationStateProvider

--- MangasBlazor/MangasBlazor/Program.cs
+++ MangasBlazor/MangasBlazor/Program.cs
@@ -1,11 +1,21 @@
+using Blazored.LocalStorage;
+using MangasBlazor;
+using MangasBlazor.Services.Authentication;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MangasBlazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("MangasAPI", options =>
+{
+    options.BaseAddress = new Uri("https://localhost:7216/");
+});
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
 
 await builder.Build().RunAsync();
